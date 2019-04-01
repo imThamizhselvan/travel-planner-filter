16dbdcee19cd536455b2f77ac76f870877a87adc
thamizhselvan <thamizhtechie@gmail.com>
2019-04-01 12:18:11 +0530

    adding data to the layout

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
 import Title from '../../components/Title';
-import { MainHeading } from './styles';
+import data from '../../data.json';
+import { MainHeading, Wrapper, SubWrapper, ListItem, Item } from './styles';
 
 class Home extends Component {
-  render() {
-    return (
-      <div>
-        <MainHeading> title </MainHeading>
-        <Title text="Destinations" />
-        <Title text="Themed vacations" />
 
-      </div>
+  render() {
+    const uniqueDestination = data.destinations.filter((destination, index, self) =>
+      index === self.findIndex((t) => (
+        t.text === destination.text
+      ))
+    )
+    return (
+      <Wrapper>
+        <MainHeading> Pickyourtrail Sitemap </MainHeading>
+        <Title text="Destinations" />
+        <hr />
+        <SubWrapper>
+          {
+            uniqueDestination.map((item, i) => (
+              <ListItem key={i}>
+                <Item href={item.url} target="_blank"> {item.text} </Item>
+              </ListItem>
+            ))
+          }
+        </SubWrapper>
+        <Title text="Themed vacations" />
+        <hr />
+        <SubWrapper>
+          {
+            data.vacations.map((item, i) => (
+              <ListItem key={i}>
+                <Item href={item.url} target="_blank"> {item.text} </Item>
+              </ListItem>
+            ))
+          }
+        </SubWrapper>
+        <Title text="Showing all pages" />
+        <hr />
+        <SubWrapper>
+          {
+            data.vacations.map((item, i) => (
+              <ListItem key={i}>
+                <Item href={item.url} target="_blank"> {item.text} </Item>
+              </ListItem>
+            ))
+          }
+        </SubWrapper>
+      </Wrapper>
     );
   }
 }
--- src/containers/Home/styles.js
+++ src/containers/Home/styles.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
 export const MainHeading = styled.h1`
-  font-color: #f2f2f2;
+  color: #736e6e;
+  margin-bottom: 40px;
+`;
+
+export const Wrapper = styled.div`
+  margin-left: 8%;
+  margin-right: 8%;
+`;
+
+export const SubWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const ListItem = styled.p`
+  margin: 20px;
+  min-width: 200px;
+`;
+
+export const Item = styled.a`
+  text-decoration: none;
+  color: gray;
+  text-transform: uppercase;
 `;
